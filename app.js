e25ee0338c0d0b407901a28bd8635b57f18d4359
Raja Saikia <rajasaikiaaa@gmail.com>
2025-07-31 13:17:37 +0530

    improve ui/ux

--- app.js
+++ app.js
@@ -1,111 +1,59 @@
-//accessing all the boxes(buttons);
-let boxes = document.querySelectorAll(".box");
-//accessing the reset button
-let resetBtn = document.querySelector("#reset-btn");
-let newGameBtn = document.querySelector("#new-btn");
-let msgContainer = document.querySelector(".msg-container");
-let msg = document.querySelector("#msg");
-let main = document.querySelector("main");
-let count = 0;
-// 1st we have to track whether it's X player or O player;
-let turnO = true; // tracking the O player turn;
-// storing the winning patterns using arrays;
-const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
+const boxes = document.querySelectorAll('.box');
+const msg = document.getElementById('msg');
+const msgContainer = document.querySelector('.msg-container');
+const newBtn = document.getElementById('new-btn');
+const resetBtn = document.getElementById('reset-btn');
+
+let currentPlayer = 'X';
+let gameBoard = ['', '', '', '', '', '', '', '', ''];
+let gameActive = true;
+
+const winningCombinations = [
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+    [0, 4, 8], [2, 4, 6] // Diagonals
 ];
 
-// adding event listeners to every boxes;
-console.log(boxes);
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        if(turnO){ //playerO
-            box.innerText = "O";
-            turnO = false;
-            box.style.color = "red";
-        } else { //playerX
-            box.innerText = "X";
-            turnO = true;
-            box.style.color = "green";
+function handleBoxClick(event) {
+    const index = event.target.dataset.index;
+    if (gameBoard[index] === '' && gameActive) {
+        gameBoard[index] = currentPlayer;
+        event.target.textContent = currentPlayer;
+        event.target.classList.add(currentPlayer.toLowerCase(), 'disabled');
+        
+        if (checkWin()) {
+            msg.textContent = `Player ${currentPlayer} Wins!`;
+            msgContainer.classList.remove('hide');
+            gameActive = false;
+        } else if (gameBoard.every(cell => cell !== '')) {
+            msg.textContent = "It's a Draw!";
+            msgContainer.classList.remove('hide');
+            gameActive = false;
+        } else {
+            currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+            msg.textContent = `Player ${currentPlayer}'s Turn`;
         }
-        box.disabled = true;
+    }
+}
 
-        checkWinner();
+function checkWin() {
+    return winningCombinations.some(combination => {
+        return combination.every(index => gameBoard[index] === currentPlayer);
     });
-});
+}
 
-const disableBoxes = () => {
-    for (let box of boxes){
-        box.disabled = true;
-    }
-};
-const enableBoxes = () => {
-    for (let box of boxes){
-        box.disabled = false;
-        box.innerText = "";
-        main.classList.remove("hide");
-    }
-};
-const showWinner = (winner) => {
-    msg.innerText = `Hurry, ${winner} is the Winner`;
-    msgContainer.classList.remove("hide");
-    msgContainer.classList.remove("red");
-    disableBoxes();
-    main.classList.add("hide");
-};
+function resetGame() {
+    gameBoard = ['', '', '', '', '', '', '', '', ''];
+    currentPlayer = 'X';
+    gameActive = true;
+    msg.textContent = `Player ${currentPlayer}'s Turn`;
+    msgContainer.classList.add('hide');
+    boxes.forEach(box => {
+        box.textContent = '';
+        box.classList.remove('x', 'o', 'disabled');
+    });
+}
 
-const checkWinner = () => {
-    for( let patterns of winPatterns) {
-        let pos1Val = boxes[patterns[0]].innerText;
-        let pos2Val = boxes[patterns[1]].innerText;
-        let pos3Val = boxes[patterns[2]].innerText;
-
-        if (pos1Val != "" && pos2Val != "" && pos3Val != ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val){
-                showWinner(pos1Val);
-            }
-        }
-    }
-};
-
-const resetGame = () => {
-    turnO = true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
-    count = 0;
-};
-
-newGameBtn.addEventListener("click", resetGame);
-resetBtn.addEventListener("click", resetGame);
-
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        count++;
-        console.log(count);
-        checkCount();
-})});
-const checkCount = () => {
-    if(count === 9){
-        showDraw();
-    }};
-
-
-const showDraw = () => {
-    msg.innerText = `Game is Draw , Restart the game`;
-    msgContainer.classList.remove("hide");
-    msgContainer.classList.add("red");
-    disableBoxes();
-    main.classList.add("hide");
-};
-
-// boxes.forEach((box) => {
-//     box.addEventListener("click", () => {
-//         if()
-//     })
-// })
+boxes.forEach(box => box.addEventListener('click', handleBoxClick));
+newBtn.addEventListener('click', resetGame);
+resetBtn.addEventListener('click', resetGame);
